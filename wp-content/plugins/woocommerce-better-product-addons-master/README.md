fdcb824279be8fcd7d4d80103adcce6678773817
yi-hsian lee <detective05@gmail.com>
2015-03-30 02:32:59 +0800

    add-on

---
+++ wp-content/plugins/woocommerce-better-product-addons-master/README.md
@@ -0,0 +1,12 @@
+woocommerce-better-product-addons
+=================================
+
+Fork of woothemes product addons plugin
+
+Changes from the original addon:
+
+ * Show options also in archive and let add to cart directly from here
+ * Removed auto-update
+ * let's the first option added in select type be the default selected
+
+
--- wp-content/themes/une_boutique-child/woocommerce/content-single-product.php
+++ wp-content/themes/une_boutique-child/woocommerce/content-single-product.php
@@ -23,7 +23,7 @@ global $post, $product, $woocommerce;
 			*
 			* @hooked woocommerce_show_messages - 10
 			*/
-			do_action( 'woocommerce_before_single_product' );
+			//do_action( 'woocommerce_before_single_product' );
 		?>
 		<div class="wpb_single_image wpb_content_element vc_align_left custom-img">
 			<div class="wpb_wrapper">
@@ -47,7 +47,7 @@ global $post, $product, $woocommerce;
 		</div>
 		</div>
 		</div>
-		<div class="relative columns vc_col-sm-6 page_column wpb_column column_container" style="padding-top:120px;">
+		<div class="relative columns vc_col-sm-6 wpb_column column_container" style="padding-top:120px;"> 
 		<div class="wpb_wrapper">
 		<div class="summary entry-summary">
 			<h3 itemprop="name" class="product_title entry-title" style="color: #000000;"><?php the_title(); ?></h3>
@@ -103,27 +103,7 @@ global $post, $product, $woocommerce;
 		 */
 		//do_action( 'woocommerce_after_single_product_summary' );
 	?>
-<div class="wpb_row row page_row menu vc_custom_1427217854488 full" style="margin:0px" data-row-effect-mobile-disable="true" data-img-parallax-mobile-disable="false" style="background-image: none !important;"><div class="upb_row_bg vcpb-default" data-upb_br_animation="" data-parallax_sense="30" data-bg-override="0" data-bg-animation="left-animation" data-bg-animation-type="h" data-animation-repeat="repeat" style="min-height: 867px; min-width: 1358px; left: 0px; width: 1358px; background-image: url(http://localhost/GreenHour/wp-content/uploads/2015/03/wood_old_0048_02_preview.jpg); background-attachment: fixed; background-color: rgba(0, 0, 0, 0); background-size: contain; background-repeat: repeat;"></div>
-	<div class="relative columns vc_col-sm-12 wpb_column column_container " >
-		<div class="wpb_wrapper">
-			<h5 style="text-align: center;  margin-bottom:35px;"><span style="border-bottom: 3px solid #000; color: #000000;">MENU</span></h5>
-			<div class="wpb_tabs wpb_content_element" data-interval="0">
-				<div class="wpb_wrapper wpb_tour_tabs_wrapper ui-tabs clearfix ui-widget ui-widget-content ui-corner-all">
-					<ul class="wpb_tabs_nav ui-tabs-nav clearfix ui-helper-reset ui-helper-clearfix ui-widget-header ui-corner-all" role="tablist"><li class="ui-state-default ui-corner-top ui-tabs-active ui-state-active" role="tab" tabindex="0" aria-controls="tab-1427215865060-2-8" aria-labelledby="ui-id-1" aria-selected="true" aria-expanded="true"><a href="#tab-1427215865060-2-8" class="ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-1">Salad</a></li><li class="ui-state-default ui-corner-top" role="tab" tabindex="-1" aria-controls="tab-1427215036-2-37" aria-labelledby="ui-id-2" aria-selected="false" aria-expanded="false"><a href="#tab-1427215036-2-37" class="ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-2">Drink</a></li><li class="ui-state-default ui-corner-top" role="tab" tabindex="-1" aria-controls="tab-1427215036-1-42" aria-labelledby="ui-id-3" aria-selected="false" aria-expanded="false"><a href="#tab-1427215036-1-42" class="ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-3">Soup</a></li></ul>
-						<div id="tab-1427215865060-2-8" class="wpb_tab ui-tabs-panel wpb_ui-tabs-hide clearfix ui-widget-content ui-corner-bottom" aria-labelledby="ui-id-1" role="tabpanel" aria-hidden="false" style="display: block;">
-							<?php echo do_shortcode( "[vc_row_inner][vc_column_inner width='1/1'][ultimate_carousel slider_type='horizontal' slide_to_scroll='single' slides_on_desk='1' slides_on_tabs='1' slides_on_mob='1' infinite_loop='on' speed='300' autoplay='off' autoplay_speed='5000' arrows='show' arrow_style='default' border_size='2' arrow_color='#333333' arrow_size='24' next_icon='ultsl-arrow-right4' prev_icon='ultsl-arrow-left4' dots='show' dots_color='#333333' dots_icon='ultsl-record' draggable='off' touch_move='off' item_space='15'][vc_column_text][product_category category='salad' per_page='6' columns='3' orderby='date' order='desc'][/vc_column_text][vc_column_text][products ids='25, 27, 80, 82, 83, 159' per_page='6' columns='3' orderby='date' order='desc' pagination='yes'][/vc_column_text][/ultimate_carousel][/vc_column_inner][/vc_row_inner]" ); ?>
-						</div>
-						<div id="tab-1427215036-2-37" class="wpb_tab ui-tabs-panel wpb_ui-tabs-hide clearfix ui-widget-content ui-corner-bottom" aria-labelledby="ui-id-2" role="tabpanel" aria-hidden="true" style="display: none;">
-							<?php echo do_shortcode("[vc_column_text][product_category category='drink' per_page='6' columns='3' orderby='date' order='desc' pagination='yes'][/vc_column_text]"); ?>
-						</div>
-						<div id="tab-1427215036-1-42" class="wpb_tab ui-tabs-panel wpb_ui-tabs-hide clearfix ui-widget-content ui-corner-bottom" aria-labelledby="ui-id-3" role="tabpanel" aria-hidden="true" style="display: none;">
-							<?php echo do_shortcode("[vc_column_text][product_category category='salad' per_page='6' columns='3' orderby='date' order='desc' pagination='yes'][/vc_column_text]"); ?>
-						</div>
-				</div> 
-			</div> 
-		</div> 
-	</div> 
-</div>
+	<?php echo do_shortcode('[greenhour_menu]'); ?>
 </div><!-- #product-<?php the_ID(); ?> -->
 
 <?php do_action( 'woocommerce_after_single_product' ); ?>
